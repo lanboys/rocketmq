b20158f5a6675cb7fc5c4512786d21ae67598609
lanboys <lan_bing2013@163.com>
2024-08-14 10:49:50 +0800

    标记 测试PUSH消费者

--- example/src/main/java/org/apache/rocketmq/example/simple/PushConsumer.java
+++ example/src/main/java/org/apache/rocketmq/example/simple/PushConsumer.java
@@ -23,54 +23,55 @@ import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.example.utils.PrintUtil;
 
 import java.util.List;
 
+/**
+ * 常用消费者客户端
+ */
 public class PushConsumer {
 
     public static void main(String[] args) throws InterruptedException, MQClientException {
         final DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("a-group");
-        consumer.setNamesrvAddr("localhost:9876");
+        consumer.setNamesrvAddr("localhost:19876");
         consumer.subscribe("aaaaa", "*");
-        //consumer.subscribe("bbbbb", "*");// 订阅多个主题
+        // consumer.subscribe("bbbbb", "*");// 订阅多个主题
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-        //wrong time format 2017_0422_221800
-        //consumer.setConsumeTimestamp("20170422221800");
-        consumer.setPullBatchSize(5);
-        //consumer.setConsumeMessageBatchMaxSize(5);
-        //consumer.setPullInterval(20000);
+        // wrong time format 2017_0422_221800
+        // consumer.setConsumeTimestamp("20170422221800");
+        consumer.setPullBatchSize(1);
+        // consumer.setConsumeMessageBatchMaxSize(5);
+        // consumer.setPullInterval(20000);
         consumer.setPullThresholdForQueue(100);
         consumer.registerMessageListener(new MessageListenerConcurrently() {
 
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-                //System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                // System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
 
                 System.out.println(Thread.currentThread().getName() + " 收到 " + msgs.size() + " 条消息");
-                context.setAckIndex(-1);//设置消费成功的索引
+                context.setAckIndex(-1);// 设置消费成功的索引
                 for (int i = 0; i < msgs.size(); i++) {
                     try {
-                        Thread.sleep(10000);
+                        Thread.sleep(3000);
                         MessageExt msg = msgs.get(i);
-                        byte[] body = msg.getBody();
-                        //if (i == 4) {
-                        //    System.out.println(Thread.currentThread().getName() + " 消费失败：" + new String(body) + " " + msg.getKeys()
-                        //        + " queueId: " + msg.getQueueId() + " queueOffset: " + msg.getQueueOffset() + " reconsumeTimes: " + msg.getReconsumeTimes() + " topic: " + msg.getTopic());
-                        //    throw new RuntimeException("模拟消息消费失败");
-                        //}
-                        System.out.println(Thread.currentThread().getName() + " 消费成功：" + new String(body) + " " + msg.getKeys()
-                            + " queueId: " + msg.getQueueId() + " queueOffset: " + msg.getQueueOffset() + " reconsumeTimes: " + msg.getReconsumeTimes() + " topic: " + msg.getTopic());
-                        context.setAckIndex(i);//设置消费成功的索引
+                        // if (4 == 4) {
+                        //     PrintUtil.printMessage(msg);
+                        //     throw new RuntimeException("模拟消息消费失败");
+                        // }
+                        PrintUtil.printMessage(msg);
+                        context.setAckIndex(i);// 设置消费成功的索引
                     } catch (Exception e) {
                         return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
                     }
                 }
 
-                //return ConsumeConcurrentlyStatus.RECONSUME_LATER; 效果等于 ackIndex == -1
+                // return ConsumeConcurrentlyStatus.RECONSUME_LATER; 效果等于 ackIndex == -1
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
-        consumer.setNamesrvAddr("localhost:9876");
+        consumer.setNamesrvAddr("localhost:19876");
         consumer.start();
         System.out.printf("Consumer Started.%n");
     }
